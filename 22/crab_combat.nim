03103ff4d03ebeac4dd52adbbd84460b16c9e5a5
Chris Le Sueur <c.m.lesueur@gmail.com>
2020-12-22 22:40:18 +0000

    Day 22: test deques

--- 22/crab_combat.nim
+++ 22/crab_combat.nim
@@ -1,29 +1,50 @@
+import deques
+import hashes
 import os
 import sequtils
 import strutils
 import sets
+import nimprof
 
-proc popFirst[T](s: var seq[T]): T =
-  result = s[0]
-  s.delete 0
+proc `&`[T](d: Deque[T], s: openArray[T]): Deque[T] =
+  result = d
+  for x in s:
+    result.addLast(x)
 
-proc score(s: seq[int]): int =
+proc add[T](d: var Deque[T], s: openArray[T]) =
+  for x in s:
+    d.addLast(x)
+
+proc hash[T](d: Deque[T]): Hash =
+  result = 0
+  for x in d:
+    result = result !& x
+  result = !$result
+
+proc `[]`[T](d: Deque[T], r: HSlice[int, int]): Deque[T] =
+  result = d
+  result.shrink(r.a, d.len - r.b - 1)
+  #result = initDeque[T](r.b - r.a)
+  #for i in r:
+    #result.addLast(d[i])
+
+proc score(s: Deque[int]): int =
   if s.len == 0:
     return 0
   toSeq(s.pairs).mapIt((s.len - it[0]) * it[1]).foldl(a + b)
 
 ### PART 1 ###
 
-proc round(deck1: var seq[int], deck2: var seq[int]) =
+proc round(deck1: var Deque[int], deck2: var Deque[int]) =
   let
     c1 = deck1.popFirst
     c2 = deck2.popFirst
   if c1 > c2:
-    deck1 = deck1 & @[c1, c2]
+    deck1 &= @[c1, c2]
   else:
-    deck2 = deck2 & @[c2, c1]
+    deck2 &= @[c2, c1]
 
-proc game(deck1: seq[int], deck2: seq[int]) =
+proc game(deck1: Deque[int], deck2: Deque[int]) =
   var
     d1 = deck1
     d2 = deck2
@@ -34,10 +55,10 @@ proc game(deck1: seq[int], deck2: seq[int]) =
 
 ### PART 2 ###
 
-proc recursiveRound(deck1: var seq[int], deck2: var seq[int])
+proc recursiveRound(deck1: var Deque[int], deck2: var Deque[int])
 
 var gameNum = 0
-proc recursiveGame(deck1: seq[int], deck2: seq[int], first: bool = false): bool =
+proc recursiveGame(deck1: Deque[int], deck2: Deque[int], first: bool = false): bool =
   echo "Game ", gameNum
   gameNum += 1
   var
@@ -55,7 +76,7 @@ proc recursiveGame(deck1: seq[int], deck2: seq[int], first: bool = false): bool 
     echo d1.score + d2.score
   return d1.len > 0
 
-proc recursiveRound(deck1: var seq[int], deck2: var seq[int]) =
+proc recursiveRound(deck1: var Deque[int], deck2: var Deque[int]) =
   let
     c1 = deck1.popFirst
     c2 = deck2.popFirst
@@ -71,18 +92,18 @@ proc recursiveRound(deck1: var seq[int], deck2: var seq[int]) =
 
   if p1win:
     echo "Player 1 wins"
-    deck1 = deck1 & @[c1, c2]
+    deck1 &= @[c1, c2]
   else:
     echo "Player 2 wins"
-    deck2 = deck2 & @[c2, c1]
+    deck2 &= @[c2, c1]
 
 ### SETUP ###
 
 let
   input = readFile(paramStr(1)).strip().split("\n\n").mapIt(it.splitlines)
 var
-  deck1 = input[0][1..^1].map(parseInt)
-  deck2 = input[1][1..^1].map(parseInt)
+  deck1 = input[0][1..^1].map(parseInt).toDeque
+  deck2 = input[1][1..^1].map(parseInt).toDeque
 
 game(deck1, deck2)
 discard recursiveGame(deck1, deck2, true)
